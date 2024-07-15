a6a37d60bc8b32a61770177b94948af5667fa211
jylups <jylups@gmail.com>
2024-07-15 17:02:12 -0400

    added roles

---
+++ AmeriCorps.Users.Data.Core/Model/Role.cs
@@ -0,0 +1,22 @@
+﻿namespace AmeriCorps.Users.Data.Core;
+
+
+public sealed class Role : Entity
+{
+    /**
+     * Role name in the format of NAME1_NAME2
+     */
+    public required string RoleName { get; set; }
+    /**
+* Human redeable name for display purposes
+*/
+    public required string FucntionalName { get; set; }
+    /**
+     * Optional summary of what the role means 
+     */
+    public string Description { get; set; } = string.Empty;
+
+    public List<User> Users { get; set; } = new List<User>();
+
+}
+
