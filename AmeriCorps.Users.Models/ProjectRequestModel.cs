621c650d4ec742562580b9525230e5201d0d4b6c
Steven Barker <Steven_Barker@scc.senate.gov>
2024-10-15 17:38:17 -0400

    clean up

--- AmeriCorps.Users.Models/ProjectRequestModel.cs
+++ AmeriCorps.Users.Models/ProjectRequestModel.cs
@@ -11,8 +11,6 @@ public class ProjectRequestModel
     public string ProgramName { get; set; } = string.Empty;
     public string ProgramYear { get; set; } = string.Empty;
 
-    // public AwardResponse? Award { get; set; } = new AwardResponse();
-
     public string StreetAddress { get; set; } = string.Empty;
     public string City { get; set; } = string.Empty;
     public string State { get; set; } = string.Empty;
