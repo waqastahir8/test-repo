a62d087774691ead4437eedb5d209bda252ea64f
Steven Barker <Steven_Barker@scc.senate.gov>
2024-10-18 15:24:52 -0400

    clean up

--- AmeriCorps.Users.Data.Core/Model/Project.cs
+++ AmeriCorps.Users.Data.Core/Model/Project.cs
@@ -1,7 +1,6 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
-using Npgsql.EntityFrameworkCore.PostgreSQL;
 
 namespace AmeriCorps.Users.Data.Core;
 
