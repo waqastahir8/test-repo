013f68546b1710be23a2a9785bfbc4c9fbeda02e
Steven Barker <Steven_Barker@scc.senate.gov>
2024-10-18 15:02:23 -0400

    APDP-948 project search

---
+++ AmeriCorps.Users.Models/SearchFilters.cs
@@ -0,0 +1,11 @@
+
+namespace AmeriCorps.Users.Models;
+
+public sealed class SearchFilters
+{
+    public string OrgCode { get; set; } = string.Empty;
+    public string Query { get; set; } = string.Empty;
+    public bool Awarded { get; set; } = true;
+    public bool Active { get; set; } = true;
+    public int ProjectId { get; set; }
+}
